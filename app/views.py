69ceaf2ae698e08beeddb10ecb3368fcb81fb119
Taylor Barnett <taylormbarnett@utexas.edu>
2014-01-10 17:06:23 -0600

    Working links except upload link causes 404

--- app/views.py
+++ app/views.py
@@ -6,3 +6,17 @@ from app import app
 @app.route('/')
 def index():
 		return render_template('index.html')
+
+@app.route('/upload', methods=['GET','POST'])
+def upload_song():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file.filename:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('index.html', filename=filename)
+    return render_template('index.html')
+ 
+@app.route('/uploads/')
+def uploaded_song(filename):
+    return send_from_directory(app.config ['UPLOAD_FOLDER'],filename, as_attachment=True)
